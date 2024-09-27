2ad28dd12a34dd3591e21038ac3ede5967eb9921
Zixi Qiao <qiaozixi@gmail.com>
2024-09-27 11:00:41 -0400

    added some test cases for returnCopy()

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,6 +1,19 @@
 public class MyArrays {
   public static void main(String[] args) {
-    
+    int[] arr1 = {1, 2, 3};
+    int[] arr2 = returnCopy(arr1);
+    System.out.println("Expected: True; Result: " + arrayToString(arr1).equals(arrayToString(arr2)) + "; ");
+    System.out.println("Expected: False; Result: " + arr1 == arr2);
+
+    arr1 = {1, 2, 5};
+    System.out.println("Expected: False; Result: " + arrayToString(arr1).equals(arrayToString(arr2)) + "; ");
+    System.out.println("Expected: False; Result: " + arr1 == arr2);
+
+    arr1 = {};
+    arr2 = returnCopy(arr1);
+    System.out.println("Expected: False; Result: " + arrayToString(arr1).equals(arrayToString(arr2)) + "; ");
+    System.out.println("Expected: False; Result: " + arr1 == arr2);
+
   }
   public static String arrayToString(int[] nums) {
     String s = "[";
@@ -13,5 +26,11 @@ public class MyArrays {
       s += "]";
     }
     return s;
+  }
+  public static int[] returnCopy(int[]ary) {
+
+  }
+  public static int[] concatArray(int[]ary1,int[]ary2) {
+
   }
 }
