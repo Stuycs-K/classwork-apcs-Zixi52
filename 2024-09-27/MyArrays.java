2f835ab3e1339a43c166ed8e5d3d66a09cc32b7d
Zixi Qiao <qiaozixi@gmail.com>
2024-09-28 12:26:11 -0400

    fixed arrayToString test cases and added concatArray test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,19 +1,34 @@
 public class MyArrays {
   public static void main(String[] args) {
-    int[] arr1 = {1, 2, 3};
+    int[] arr1 = new int[]{1, 2, 3};
     int[] arr2 = returnCopy(arr1);
     System.out.println("Expected: True; Result: " + arrayToString(arr1).equals(arrayToString(arr2)) + "; ");
-    System.out.println("Expected: False; Result: " + arr1 == arr2);
+    System.out.println("Expected: False; Result: " + (arr1 == arr2));
 
-    arr1 = {1, 2, 5};
+    arr1 = new int[]{1, 2, 5};
     System.out.println("Expected: False; Result: " + arrayToString(arr1).equals(arrayToString(arr2)) + "; ");
-    System.out.println("Expected: False; Result: " + arr1 == arr2);
+    System.out.println("Expected: False; Result: " + (arr1 == arr2));
 
-    arr1 = {};
+    arr1 = new int[0];
     arr2 = returnCopy(arr1);
-    System.out.println("Expected: False; Result: " + arrayToString(arr1).equals(arrayToString(arr2)) + "; ");
-    System.out.println("Expected: False; Result: " + arr1 == arr2);
+    System.out.println("Expected: True; Result: " + arrayToString(arr1).equals(arrayToString(arr2)) + "; ");
+    System.out.println("Expected: False; Result: " + (arr1 == arr2));
 
+    arr1 = new int[]{-1, -105, 302, 17, 52};
+    arr2 = returnCopy(arr1);
+    System.out.println("Expected: True; Result: " + arrayToString(arr1).equals(arrayToString(arr2)) + "; ");
+    System.out.println("Expected: False; Result: " + (arr1 == arr2));
+
+    System.out.println("/nExpected: {-1, -105, 302, 17, 52, -1, -105, 302, 17, 52}; Result: " + concatArray(arr1, arr2));
+
+    arr1 = new int[0];
+    System.out.println("/nExpected: {-1, -105, 302, 17, 52}; Result: " + concatArray(arr1, arr2));
+
+    arr1 = new int[]{12, -5, 3};
+    System.out.println("/nExpected: {12, -5, 3, -1, -105, 302, 17, 52}; Result: " + concatArray(arr1, arr2));
+
+    arr2 = new int[0];
+    System.out.println("/nExpected: {12, -5, 3}; Result: " + concatArray(arr1, arr2));
   }
   public static String arrayToString(int[] nums) {
     String s = "[";
