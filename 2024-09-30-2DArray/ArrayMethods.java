eae5a0e025e9d78426c84fbbf5791919fa38fc39
Zixi Qiao <qiaozixi@gmail.com>
2024-09-30 11:00:45 -0400

    coded arrToString(int[][] ary) and fixed test cases

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -3,30 +3,30 @@
 
 public static void main(String[] args) {
   int[][] ary = new int[][]{{1, 2, 3}, {5, 6, 7}, {8, 9, 10}};
-  System.out.println("Expected: [[1, 2, 3], [5, 6, 7], [8, 9, 10]]; Result: "  + arrToString(int[][] ary));
+  System.out.println("Expected: [[1, 2, 3], [5, 6, 7], [8, 9, 10]]; Result: "  + arrToString(ary));
 
   ary = new int[][]{{1, -2}, {5, 6, 7, 9, 1023024}, {}};
-  System.out.println("Expected: [[1, -2], [5, 6, 7, 9, 1023024], []]; Result: "  + arrToString(int[][] ary));
+  System.out.println("Expected: [[1, -2], [5, 6, 7, 9, 1023024], []]; Result: "  + arrToString(ary));
 
   ary = new int[][]{{}, {}, {}};
-  System.out.println("Expected: [[], [], []]; Result: "  + arrToString(int[][] ary));
+  System.out.println("Expected: [[], [], []]; Result: "  + arrToString(ary));
 
 
   ary = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9, 10}};
-  System.out.println("Expected: 55; Result: "  + arr2DSum(int[][] ary));
+  System.out.println("Expected: 55; Result: "  + arr2DSum(ary));
 
   ary = new int[][]{{}, {}, {}};
-  System.out.println("Expected: 0; Result: "  + arr2DSum(int[][] ary));
+  System.out.println("Expected: 0; Result: "  + arr2DSum(ary));
 
   ary = new int[][]{{2}, {5}, {-1}};
-  System.out.println("Expected: 6; Result: "  + arr2DSum(int[][] ary));
+  System.out.println("Expected: 6; Result: "  + arr2DSum(ary));
 
 
   ary = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-  System.out.println("Expected: [[1, 4, 7], [2, 5, 8], [3, 6, 9]]; Result: "  + swapRC(int[][] ary));
+  System.out.println("Expected: [[1, 4, 7], [2, 5, 8], [3, 6, 9]]; Result: "  + swapRC(ary));
 
   ary = new int[][]{{-2, 3}, {1, 5}, {-2, 3}};
-  System.out.println("Expected: [[-2, 1, -2], [3, 5, 3]]; Result: "  + swapRC(int[][] ary));
+  System.out.println("Expected: [[-2, 1, -2], [3, 5, 3]]; Result: "  + swapRC(ary));
 
 }
 //2. Copy your arrToString method from before.
@@ -58,12 +58,21 @@ public static String arrToString(int[]ary){
   */
 public static String arrToString(int[][]ary){
   //this should use arrToString(int[])
-  return "";
+  String s = "[";
+  for (int i = 0; i < ary.length; i++) {
+    if (i = ary.length - 1) {
+      s += arrToString(ary[i]);
+    } else {
+      s = arrToString(ary[i]) + ", ";
+    }
+  }
+  return s + "]";
 }
 
 /*Return the sum of all of the values in the 2D array */
 public static int arr2DSum(int[][]nums){
   //use a nested loop to solve this
+  return 1;
 }
 
 /**Rotate an array by returning a new array with the rows and columns swapped.
