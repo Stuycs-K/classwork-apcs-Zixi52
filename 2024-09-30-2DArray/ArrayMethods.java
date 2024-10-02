1866d3ac3b0a0c7608815d1c5e3a904f783bfba8
Zixi Qiao <qiaozixi@gmail.com>
2024-10-01 20:10:12 -0400

    coded replaceNegative() and test cases

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -2,6 +2,9 @@
 //   Zixi Qiao, zqiao60@stuy.edu
 public class ArrayMethods {
   public static void main(String[] args) {
+
+    //arrToString test cases
+
     int[][] ary = new int[][]{{1, 2, 3}, {5, 6, 7}, {8, 9, 10}};
     System.out.println("Expected: [[1, 2, 3], [5, 6, 7], [8, 9, 10]]; Result: "  + arrToString(ary));
 
@@ -11,6 +14,7 @@ public class ArrayMethods {
     ary = new int[][]{{}, {}, {}};
     System.out.println("Expected: [[], [], []]; Result: "  + arrToString(ary));
 
+    //arr2DSum test cases
 
     ary = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9, 10}};
     System.out.println("Expected: 55; Result: "  + arr2DSum(ary));
@@ -21,6 +25,7 @@ public class ArrayMethods {
     ary = new int[][]{{2}, {5}, {-1}};
     System.out.println("Expected: 6; Result: "  + arr2DSum(ary));
 
+    //swapRC test cases
 
     ary = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     System.out.println("Expected: [[1, 4, 7], [2, 5, 8], [3, 6, 9]]; Result: "  + arrToString(swapRC(ary)));
@@ -28,12 +33,23 @@ public class ArrayMethods {
     ary = new int[][]{{-2, 3}, {1, 5}, {-2, 3}};
     System.out.println("Expected: [[-2, 1, -2], [3, 5, 3]]; Result: "  + arrToString(swapRC(ary)));
 
+    //replaceNegative test cases
+
+    ary = new int[][]{{1, -2, 3}, {-4, 5, -6}, {7, -8, -9}};
+    System.out.println("Original: " + arrToString(ary) +"; Result: ");
+    replaceNegative(ary);
+    System.out.print(arrToString(ary));
+  
+    ary = new int[][]{{}, {-4, 5}, {7, -8, -9}};
+    System.out.println("Original: " + arrToString(ary) +"; Result: ");
+    replaceNegative(ary);
+    System.out.print(arrToString(ary));
   }
-//2. Copy your arrToString method from before.
-/**Return a String that represets the array in the format:
-* "[2, 3, 4, 9]"
-* Note the comma+space between values, and between values
-*/
+  //2. Copy your arrToString method from before.
+  /**Return a String that represets the array in the format:
+    * "[2, 3, 4, 9]"
+    * Note the comma+space between values, and between values
+  */
 
   public static String arrToString(int[]nums){
     String s = "[";
@@ -48,14 +64,14 @@ public class ArrayMethods {
     return s;
   }
 
-//3. Write arrToString, with a 2D array parameter.
-//Note: Different parameters are allowed with the same function name.
-/**Return a String that represets the 2D array in the format:
-  * "[[2, 3, 4], [5, 6, 7], [2, 4, 9]]"
-  * Note the comma+space between values, and between arrays
-  * You are encouraged to notice that you may want to re-use
-  * previous code, but you should NOT duplicate that code. (Don't copy/paste or retype it)
-  */
+  //3. Write arrToString, with a 2D array parameter.
+  //Note: Different parameters are allowed with the same function name.
+  /**Return a String that represets the 2D array in the format:
+    * "[[2, 3, 4], [5, 6, 7], [2, 4, 9]]"
+    * Note the comma+space between values, and between arrays
+    * You are encouraged to notice that you may want to re-use
+    * previous code, but you should NOT duplicate that code. (Don't copy/paste or retype it)
+    */
   public static String arrToString(int[][]ary){
     //this should use arrToString(int[])
     String s = "[";
@@ -69,7 +85,7 @@ public class ArrayMethods {
     return s + "]";
   }
 
-/*Return the sum of all of the values in the 2D array */
+  /*Return the sum of all of the values in the 2D array */
   public static int arr2DSum(int[][]nums){
     //use a nested loop to solve this
     int sum = 0;
@@ -81,9 +97,9 @@ public class ArrayMethods {
     return sum;
   }
 
-/**Rotate an array by returning a new array with the rows and columns swapped.
-  * You may assume the array is rectangular and neither rows nor cols is 0.
-  * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
+  /**Rotate an array by returning a new array with the rows and columns swapped.
+    * You may assume the array is rectangular and neither rows nor cols is 0.
+    * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
     */
   public static int[][] swapRC(int[][]nums){
     int[][] newNums = new int[nums[0].length][nums.length];
@@ -94,4 +110,32 @@ public class ArrayMethods {
     }
     return newNums;
   }
+
+  //3. Modify a given 2D array of integer as follows:
+  //Replace all the negative values:
+  //-When the row number is the same as the column number replace
+  //that negative with the value 1
+  //-All other negatives replace with 0
+  public static void replaceNegative(int[][] vals){
+    for (int i = 0; i < vals.length; i++){
+      for (int x = 0; x < vals[i].length; x++){
+        if (i == x && vals[i][x] < 0){
+          vals[i][x] = 1;
+        }
+        else if (vals[i][x] < 0){
+          vals[i][x] = 0;
+        }
+      }
+    }
+  }
+
+  //4. Make a copy of the given 2d array.
+  //When testing : make sure that changing the original does NOT change the copy.
+  //DO NOT use any built in methods that "copy" an array.
+  //You SHOULD write a helper method for this.
+  //If you don't see a good way to do that, you should stop and look at prior methods.
+  public static int[][] copy(int[][] nums){
+    return null;//placeholder so it compiles
+  }
+
 }
