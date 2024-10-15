cd18b30ce396e0e693e54b60f0bda6735a9c6e74
Zixi Qiao <qiaozixi@gmail.com>
2024-10-15 10:58:01 -0400

    created 3 points for an equilateral triangle and printed side lengths

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -16,5 +16,13 @@ public class Demo{
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
+    // equilateral triangle
+    Point t1 = new Point(0,0);
+    Point t2 = new Point(1,0);
+    Point t3 = new Point(0.5, Math.sqrt(3) / 2.0);
+    System.out.println("" + t1 + t2 + t3);
+    System.out.println(t1.distanceTo(t2));
+    System.out.println(t2.distanceTo(t3));
+    System.out.println(t1.distanceTo(t3));
   }
 }
