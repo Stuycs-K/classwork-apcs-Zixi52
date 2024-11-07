449909f76902768679a24cca42628ae6bd2ab55f
Zixi Qiao <qiaozixi@gmail.com>
2024-11-07 10:47:04 -0500

    overrode getName() in Bird and commented what happened

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,7 @@
+// When we made the change in step 7, we made a new version of getname() for the Bird subclass that firstly called the getName() from the parent class (super.getName())
+// and then appended it to a string "The Mighty ". this means that when we call speak for a bird, it uses the new getName() method and prefixes the name with "the Mighty "
+
+
 public class Driver {
   public static void main(String[] args) {
     Animal polly = new Animal("polly wanna cracker", 16, "polly");
