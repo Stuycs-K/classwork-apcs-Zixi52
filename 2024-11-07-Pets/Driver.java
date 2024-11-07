21ff2a3045bb6070abba02b0d49047e4806df0ad
Zixi Qiao <qiaozixi@gmail.com>
2024-11-07 10:40:33 -0500

    coded bird constructor, overrode speak(), and tested

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -2,5 +2,7 @@ public class Driver {
   public static void main(String[] args) {
     Animal polly = new Animal("polly wanna cracker", 16, "polly");
     polly.speak();
+    Bird jay = new Bird("chirp", 25, "Jay", 5.0, "blue");
+    jay.speak();
   }
 }
