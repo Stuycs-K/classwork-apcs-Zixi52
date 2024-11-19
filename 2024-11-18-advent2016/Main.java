750e8f34d48c97c4b63200332af967cd83d866a4
Zixi Qiao <qiaozixi@gmail.com>
2024-11-18 20:31:29 -0500

    overhauled code and completed part one of day one

--- 2024-11-18-advent2016/Main.java
+++ 2024-11-18-advent2016/Main.java
@@ -10,39 +10,52 @@ public class Main {
 
   public static int distanceBlocks(String filename) {
     int total = 0;
+
     try {
       File file = new File(filename);//1
       Scanner scan = new Scanner(file);
       //CODE THAT SCANS THE FILE.
-      String dir = "N";
-      boolean changeDir = false;
-      int countX=0;
-      int countY=0;
-      String s = scan.next();
-      while (scan.hasNext()) {
-          if (s.charAt(0) == 'L') {
-            if (dir.equals("N")) {
-              countX += Integer.valueOf(s.substring(1,2));
-              dir = "W";
-            } else if (dir.equals("W")) {
+      int countX = 0;
+      int countY = 0;
+      // Initial direction (0: North, 1: East, 2: South, 3: West)
+      int direction = 0;
+      String s = scan.nextLine();
 
-            }
+      String[] instructions = s.split(",\\s*");
 
-          } else if (s.charAt(0) == 'R') {
-            countX += Integer.valueOf(s.substring(1,2));
-          } else if (s.charAt(0) == 'S') {
-            countY -= Integer.valueOf(s.substring(1,2));
-          } else {
-            countY += Integer.valueOf(s.substring(1,2));
-          }
+      for (int i = 0; i < instructions.length; i++) {
+        char turn = instructions[i].charAt(0);
+        int blocks = Integer.parseInt(instructions[i].substring(1));
+
+        // Update direction based on the turn
+        if (turn == 'L') {
+            direction = (direction + 3) % 4; // Turn left
+        } else if (turn == 'R') {
+            direction = (direction + 1) % 4; // Turn right
         }
-      total = countX + countY;
+
+        System.out.println("Instruction: " + instructions[i]);
+        System.out.println("Current Position: (" + countX + ", " + countY + ")");
+        System.out.println("Direction: " + direction);
+
+        if (direction == 0) {
+          countY += blocks;
+        } else if (direction == 1) {
+          countX += blocks;
+        } else if (direction == 2) {
+          countY -= blocks;
+        } else {
+          countX -= blocks;
+        }
+      }
+ 
+      total = Math.abs(countX) + Math.abs(countY);
       scan.close();//releases the file from your program
 
     } catch (FileNotFoundException ex) {
       //File not found what should you do?
       System.out.println("File not found");
-      return -1; //you can return from a void function just don't put a value.
+      return -1;
     }
     return total;
 
