574929de8c4d0b456041dae4fa9e257aac0e0527
Zixi Qiao <qiaozixi@gmail.com>
2024-11-21 21:14:34 -0500

    completed message()

--- 2024-11-18-advent2016/Day6Part1.java
+++ 2024-11-18-advent2016/Day6Part1.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -23,30 +22,44 @@ public class Day6Part1 {
       System.out.println("File not found");
       return "nope";
     }
+  }
 
     public static String message() {
       String s = parse("day6Directions.txt");
       String encodedMSG[] = s.split(",");
-      ArrayList<String> arrList = new ArrayList<String>();
-      for (int i = 0; i < encodedMSG.length; i++) {
-        ArrayList<Integer> counter  = new ArrayList<Integer>();
-        ArrayList<String> letters = new ArrayList<String>();
-        char largest = '';
-        for (int j = 0; j < encodedMSG[i].length(); j++) {
-          char c = encodedMSG[i].charAt(j);
+      String total = "";
+      ArrayList<ArrayList<Character>> arrList = new ArrayList<ArrayList<Character>>();
+      for (int col = 0; col < encodedMSG[0].length(); col++) {
+        arrList.add(new ArrayList<Character>());
+        for (int row = 0; row < encodedMSG.length; row++) {
+            arrList.get(col).add(encodedMSG[row].charAt(col));
+        }
+      }
+      for (int i = 0; i < arrList.size(); i++) {
+        ArrayList<Integer> counter = new ArrayList<Integer>();
+        ArrayList<Character> letters = new ArrayList<Character>();
+        char largest = 'c';
+        int maxCount = 0;
+        for (int j = 0; j < arrList.get(i).size(); j++) {
+          char c = arrList.get(i).get(j);
           if (!letters.contains(c)) {
             letters.add(c);
-            counter.add(c);
+            counter.add(1);
           } else {
             int index = letters.indexOf(c);
-            counter.set(index, counter.get(index));
+            counter.set(index, counter.get(index)+1);
           }
         }
-        for (int k = 0; k < counter.size()-1; k++) {
-          if (counter.get(k) > counter.get(k+1)) {
+        System.out.println(letters.toString());
+        System.out.println(counter.toString());
+        for (int k = 0; k < counter.size(); k++) {
+          if (counter.get(k) > maxCount) {
+            maxCount = counter.get(k);
             largest = letters.get(k);
           }
         }
+        total += largest;
       }
+      return total;
     }
-  }
+}
