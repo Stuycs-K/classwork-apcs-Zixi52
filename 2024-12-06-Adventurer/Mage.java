2a6da461ff17307fb6fb9bb37a6501a73d05a2cc
Zixi Qiao <qiaozixi@gmail.com>
2024-12-06 10:44:33 -0500

    coded attack() and support(other)

--- 2024-12-06-Adventurer/Mage.java
+++ 2024-12-06-Adventurer/Mage.java
@@ -2,14 +2,14 @@ public class Mage extends Adventurer{
   private int mana, maxMana;
   public Mage(String name) {
     super(name);
-    mana = 25;
-    maxMana = 100;
+    mana = 10;
+    maxMana = 20;
   }
 
   public Mage(String name, int HP) {
     super(name, HP);
-    mana = 25;
-    maxMana = 100;
+    mana = 10;
+    maxMana = 20;
   }
 
   public String getSpecialName() {
@@ -26,5 +26,25 @@ public class Mage extends Adventurer{
 
   public int getSpecialMax() {
     return maxMana;
+  }
+
+  public String attack(Adventurer other) {
+    int dmg = (int) Math.random()*5;
+    other.applyDamage(dmg);
+    return "Dealt " + dmg.toString + " damage"();
+  }
+
+  public String support(Adventurer other) {
+    int heal;
+    if ((int) Math.random()*3 == 0) {
+      heal = 1;
+    } else {
+      heal = (int) Math.random()*3;
+    }
+    if (other.getHP() + heal < other.getmaxHP()) {
+      other.setHP(other.getHP() + heal);
+    } else {
+      other.setHP(other.getmaxHP());
+    }
   }
 }
