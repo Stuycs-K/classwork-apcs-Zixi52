e39c822042e316a12830b47e0d4935b89e8404bd
Zixi Qiao <qiaozixi@gmail.com>
2024-12-06 10:33:08 -0500

    added unique fields and coded getter/setter methods

--- 2024-12-06-Adventurer/Mage.java
+++ 2024-12-06-Adventurer/Mage.java
@@ -1,12 +1,30 @@
 public class Mage extends Adventurer{
-
+  private int mana, maxMana;
   public Mage(String name) {
     super(name);
+    mana = 25;
+    maxMana = 100;
   }
 
   public Mage(String name, int HP) {
     super(name, HP);
+    mana = 25;
+    maxMana = 100;
   }
 
-  
+  public String getSpecialName() {
+    return "mana";
+  }
+
+  public int getSpecial() {
+    return mana;
+  }
+
+  public void setSpecial(int n) {
+    mana = n;
+  }
+
+  public int getSpecialMax() {
+    return maxMana;
+  }
 }
